4f615f0fb39be0acd33e6ea3c468d5ddc65470b7
marcuspietarinen <marcus.pietarinen@syk.fi>
2023-09-15 14:48:51 +0300

    moved comments from controller to main files

--- src/com/company/HomeworkController.java
+++ src/com/company/HomeworkController.java
@@ -7,15 +7,6 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-//Advice
-
-//1. Back up code to GitHub or to a separate file folder before ANY changes.
-//NOTE: do this incrementally, so try to run code after every change to verify it works and maybe commit to git.
-//2. View never touches the model, i.e. there is never a word called model in a file called view.
-//  INSTEAD, View has reference to controller and is able to call functions on controller.
-//  AND, Controller has reference to model and is able to call functions on model.
-//3. Think of every time you create something new.
-
 
 public class HomeworkController {
     private HomeworkModel model;
